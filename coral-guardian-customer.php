07605e150e3d2ae81cee6f027ddb0e2b5c2b8bf9
d4rk0s <gregory@d4rk0s.net>
2022-09-19 11:38:18 +0200

    bugfix

--- coral-guardian-customer.php
+++ coral-guardian-customer.php
@@ -9,9 +9,16 @@
  * Author URI:
  * Licence: GPLv2
  */
-add_action(\D4rk0snet\CoralCustomer\Enum\CoralCustomerActions::NEW_CUSTOMER->value, [\D4rk0snet\CoralCustomer\Action\NewCustomer::class,'doAction'], 10, 1);
-add_filter(\D4rk0snet\CoralCustomer\Enum\CoralCustomerFilters::GET_CUSTOMER->value, [\D4rk0snet\CoralCustomer\Filter\GetCustomer::class,'doAction'], 10, 2);
-add_filter(\Hyperion\Doctrine\Plugin::ADD_ENTITIES_FILTER, function (array $entityPaths) {
+
+use D4rk0snet\CoralCustomer\Action\NewCustomer;
+use D4rk0snet\CoralCustomer\Enum\CoralCustomerActions;
+use D4rk0snet\CoralCustomer\Enum\CoralCustomerFilters;
+use D4rk0snet\CoralCustomer\Filter\GetCustomer;
+use Hyperion\Doctrine\Plugin;
+
+add_action(CoralCustomerActions::NEW_CUSTOMER->value, [NewCustomer::class,'doAction'], 10, 1);
+add_filter(CoralCustomerFilters::GET_CUSTOMER->value, [GetCustomer::class,'doAction'], 10, 2);
+add_filter(Plugin::ADD_ENTITIES_FILTER, function (array $entityPaths) {
     $entityPaths[] = __DIR__."/src/Entity";
 
     return $entityPaths;
