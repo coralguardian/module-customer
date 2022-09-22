2652852ebfd54ace03af5b809757532f2f7a48ed
d4rk0s <gregory@d4rk0s.net>
2022-09-22 16:27:22 +0200

    bugfix

--- src/Action/NewCustomer.php
+++ src/Action/NewCustomer.php
@@ -6,8 +6,11 @@ use D4rk0snet\CoralCustomer\Entity\CompanyCustomerEntity;
 use D4rk0snet\CoralCustomer\Entity\CustomerEntity;
 use D4rk0snet\CoralCustomer\Enum\CustomerType;
 use D4rk0snet\CoralCustomer\Model\CustomerModel;
+use D4rk0snet\Coralguardian\Enums\Language;
+use D4rk0snet\Coralguardian\Enums\SIBLists;
 use Exception;
 use Hyperion\Doctrine\Service\DoctrineService;
+use Hyperion\Sendinblue\Service\CustomerService;
 
 /**
  * Gère l'évènement Create customer
@@ -41,6 +44,28 @@ class NewCustomer
                 $em->persist($customer);
                 $em->flush();
             }
+
+            /** @todo: a déplacer dans un module qui écoute des actions */
+            // Create sendinblue user and put it in newsletter list
+            $sibLists = [];
+            if($customerModel->wantsNewsletter() === true)
+            {
+                $sibLists[] = $customerModel->getLanguage() === Language::FR ? SIBLists::NEWSLETTER_FR->value : SIBLists::NEWSLETTER_EN->value;
+            }
+
+            CustomerService::createCustomer(
+                $customerModel->getEmail(),
+                [
+                    'NOM' => $customerModel->getLastname(),
+                    'PRENOM' => $customerModel->getFirstname(),
+                    'CODE_POSTAL' => $customerModel->getPostalCode(),
+                    'VILLE' => $customerModel->getCity(),
+                    'PAYS' => $customerModel->getCountry(),
+                    'ADRESSE' => $customerModel->getAddress(),
+                    'LANGUE' => $customerModel->getLanguage()->value
+                ],
+                $sibLists
+            );
         }
         elseif ($customerModel->getCustomerType() === CustomerType::COMPANY) {
             $customer = $em
